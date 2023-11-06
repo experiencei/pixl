e8eb5ffa17edd335fc9472a61cabeece07ae5318
experiencei <ayelojahighbee01@gmail.com>
2023-11-06 16:21:09 +0100

    working with if-else in go

---
+++ exercise/if-else/if-else.go
@@ -0,0 +1,14 @@
+//--Summary:
+//  The existing program is used to restrict access to a resource
+//  based on day of the week and user role. Currently, the program
+//  allows any user to access the resource. Implement the functionality
+//  needed to grant resource access using any combination of `if`, `else if`,
+//  and `else`.
+//
+//--Requirements:
+//* Use the accessGranted() and accessDenied() functions to display
+//  informational messages
+//* Access at any time: Admin, Manager
+//* Access weekends: Contractor
+//* Access weekdays: Member
+//* Access Mondays, Wednesdays, and Fridays: Guest
