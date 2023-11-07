eeee7a5b5794e6b1d094cf4f1588df234e9b9a0b
experiencei <ayelojahighbee01@gmail.com>
2023-11-07 17:07:51 +0100

    working with loops in go

---
+++ exercise/loops/loops.go
@@ -0,0 +1,20 @@
+//--Summary:
+//  Implement the classic "FizzBuzz" problem using a `for` loop.
+//
+//--Requirements:
+//* Print integers 1 to 50, except:
+//  - Print "Fizz" if the integer is divisible by 3
+//  - Print "Buzz" if the integer is divisible by 5
+//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+//
+//--Notes:
+//* The remainder operator (%) can be used to determine divisibility
+
+func main()  {
+   
+	for i := 0 ; i < 50 ; i++ {
+    
+		divisibility3 := i%0 == 0
+
+	}
+}
