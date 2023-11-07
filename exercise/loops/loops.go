fc476d0e5c51c5ef7fbdb3243f08519deaf544af
experiencei <ayelojahighbee01@gmail.com>
2023-11-07 17:09:43 +0100

    working with loops in go

--- exercise/loops/loops.go
+++ exercise/loops/loops.go
@@ -12,9 +12,23 @@
 
 func main()  {
    
-	for i := 0 ; i < 50 ; i++ {
-    
-		divisibility3 := i%0 == 0
+	//* Print integers 1 to 50, except:
+	for i := 1; i <= 50; i++ {
 
+		divisibleBy3 := i%3 == 0
+		divisibleBy5 := i%5 == 0
+
+		if divisibleBy3 && divisibleBy5 {
+			//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+			fmt.Println("FizzBuzz")
+		} else if divisibleBy3 {
+			//  - Print "Fizz" if the integer is divisible by 3
+			fmt.Println("Fizz")
+		} else if divisibleBy5 {
+			//  - Print "Buzz" if the integer is divisible by 5
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
 	}
 }
