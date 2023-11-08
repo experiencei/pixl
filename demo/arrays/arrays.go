29d4be3621173fe6d22f61ea31726e03749ccab3
experiencei <ayelojahighbee01@gmail.com>
2023-11-08 17:24:32 +0100

    using structs , array and loops to prac clean service aps

--- demo/arrays/arrays.go
+++ demo/arrays/arrays.go
@@ -19,3 +19,22 @@ func checkCleanliness(rooms [4]Room) {
 		}
 	}
 }
+
+
+func main() {
+	rooms := [...]Room{
+		{name: "Office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+
+	fmt.Println("Performing cleaning...")
+	// Elements start at index 0
+	rooms[2].cleaned = true // element 3
+	rooms[3].cleaned = true // element 4
+
+	checkCleanliness(rooms)
+}
