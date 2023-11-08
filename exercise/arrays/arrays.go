939402212bdb15917b2b3b584c5d91d293c8a828
experiencei <ayelojahighbee01@gmail.com>
2023-11-08 17:37:59 +0100

    using structs , array and loops to prac clean service aps

--- exercise/arrays/arrays.go
+++ exercise/arrays/arrays.go
@@ -13,3 +13,29 @@
 //  - The total cost of the items
 //* Add a fourth product to the list and print out the
 //  information again
+
+
+package main
+ 
+import "fmt"
+
+type Product struct {
+	price int
+	name  string
+}
+
+//* Print to the terminal:
+//  - The last item on the list
+//  - The total number of items
+//  - The total cost of the items
+func printStats(list [4]Product) {
+	var cost, totalItems int
+
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		cost += item.price
+
+		if item.name != "" {
+			totalItems += 1
+		}
+	}
