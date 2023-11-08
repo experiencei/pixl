11f978ec32c8a2bc82d09347c341725d01be8c5a
experiencei <ayelojahighbee01@gmail.com>
2023-11-08 16:34:09 +0100

    working with type struct in GO

---
+++ demo/structs/structs.go
@@ -0,0 +1,23 @@
+package main
+
+
+import "fmt"
+
+
+
+type Passenger struct {
+	Name         string
+	TicketNumber int
+	Boarded      bool
+}
+
+
+type Bus struct{
+	FrontSeat Passenger
+}
+
+func main() {
+	casey := Passenger{"Casey", 1, false}
+	fmt.Println(casey)
+	
+}
