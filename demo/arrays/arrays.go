a0133230371bf98cf55b449d1f968e555d0df455
experiencei <ayelojahighbee01@gmail.com>
2023-11-08 17:24:15 +0100

    using structs , array and loops to prac clean service aps

--- demo/arrays/arrays.go
+++ demo/arrays/arrays.go
@@ -6,3 +6,16 @@ type Room struct {
 	name    string
 	cleaned bool
 }
+
+
+// Must specify the number of array elements in the function parameters
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.cleaned {
+			fmt.Println(room.name, "is clean")
+		} else {
+			fmt.Println(room.name, "is dirty")
+		}
+	}
+}
