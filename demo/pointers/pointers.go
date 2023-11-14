33098b258dad182275d33126b5e891a76f555903
experiencei <ayelojahighbee01@gmail.com>
2023-11-14 16:07:01 +0100

    working with maps and pointers in Go

--- demo/pointers/pointers.go
+++ demo/pointers/pointers.go
@@ -5,3 +5,9 @@ import "fmt"
 type Counter struct {
 	hits int
 }
+
+func increment(counter *Counter) {
+	// Using dot notation automatically dereferences. No asterisk(*) needed.
+	counter.hits += 1
+	fmt.Println("Counter", counter)
+}
