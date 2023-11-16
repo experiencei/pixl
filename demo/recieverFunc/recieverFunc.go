d6a070512c65a69059d4489e7f5904e792d8b04f
experiencei <ayelojahighbee01@gmail.com>
2023-11-16 13:40:55 +0100

    working with reciever function in Go

--- demo/recieverFunc/recieverFunc.go
+++ demo/recieverFunc/recieverFunc.go
@@ -9,3 +9,13 @@ type Space struct {
 type ParkingLot struct {
 	spaces []Space
 }
+
+
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+// Same as above, different calling convention
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
