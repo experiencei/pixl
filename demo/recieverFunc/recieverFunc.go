90ac97556b409278b9ca6f8bd785472deb25f9a9
experiencei <ayelojahighbee01@gmail.com>
2023-11-16 13:41:58 +0100

    working with reciever function in Go

--- demo/recieverFunc/recieverFunc.go
+++ demo/recieverFunc/recieverFunc.go
@@ -20,6 +20,10 @@ func (lot *ParkingLot) occupySpace(spaceNum int) {
 	lot.spaces[spaceNum-1].occupied = true
 }
 
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
 
 func main() {
 	lot := ParkingLot{spaces: make([]Space, 4)}
