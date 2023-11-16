d77b4a3cf1768a299e41cdc45aeab13d582fd985
experiencei <ayelojahighbee01@gmail.com>
2023-11-16 13:28:42 +0100

    building a library review system

--- exercise/libraryReview/libraryReview.go
+++ exercise/libraryReview/libraryReview.go
@@ -30,3 +30,56 @@ type LendAudit struct {
 	checkOut time.Time
 	checkIn  time.Time
 }
+
+
+
+
+func main() {
+	library := Library{
+		books:   make(map[Title]BookEntry),
+		members: make(map[Name]Member),
+	}
+	//  - Add at least 4 books...
+	library.books["Webapps in Go"] = BookEntry{
+		total:  4,
+		lended: 0,
+	}
+	library.books["The Little Go Book"] = BookEntry{
+		total:  3,
+		lended: 0,
+	}
+	library.books["Let's learn Go"] = BookEntry{
+		total:  2,
+		lended: 0,
+	}
+	library.books["Go Bootcamp"] = BookEntry{
+		total:  1,
+		lended: 0,
+	}
+	//  ... and at least 3 members to the library
+	library.members["Jayson"] = Member{"Jayson", make(map[Title]LendAudit)}
+	library.members["Billy"] = Member{"Billy", make(map[Title]LendAudit)}
+	library.members["Susanna"] = Member{"Susanna", make(map[Title]LendAudit)}
+
+	fmt.Println("\nInitial:")
+	//  - Print out initial library information, and after each change
+	printLibraryBooks(&library)
+	printMemberAudits(&library)
+
+	//  - Check out a book
+	member := library.members["Jayson"]
+	checkedOut := checkoutBook(&library, "Go Bootcamp", &member)
+	fmt.Println("\nCheck out a book:")
+	if checkedOut {
+		printLibraryBooks(&library)
+		printMemberAudits(&library)
+	}
+
+	//  - Check in a book
+	returned := returnBook(&library, "Go Bootcamp", &member)
+	fmt.Println("\nCheck in a book:")
+	if returned {
+		printLibraryBooks(&library)
+		printMemberAudits(&library)
+	}
+}
