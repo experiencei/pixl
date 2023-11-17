395e92db4f8c88f0592088d76cbbe5cb359aa3ac
experiencei <ayelojahighbee01@gmail.com>
2023-11-17 17:10:21 +0100

    varaidics give output of slice

--- demo/variadics/variadics.go
+++ demo/variadics/variadics.go
@@ -9,3 +9,17 @@ func sum(nums ...int) int {
 	}
 	return sum
 }
+
+
+func main() {
+	a := []int{1, 2, 3}
+	b := []int{4, 5, 6}
+
+	all := append(a, b...)
+
+	answer := sum(all...)
+	fmt.Println(answer)
+
+	answer = sum(1, 2, 3, 4, 5, 6)
+	fmt.Println(answer)
+}
