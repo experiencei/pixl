58b29502d29d314c91f250b12e6633d134fbe3eb
experiencei <ayelojahighbee01@gmail.com>
2023-11-28 23:18:34 +0100

    working with interfaces in go

--- demo/interfaces/interfaces.go
+++ demo/interfaces/interfaces.go
@@ -17,3 +17,13 @@ func (s Salad) PrepareDish() {
 	fmt.Println("chop salad")
 	fmt.Println("add dressing")
 }
+
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("--Dish: %v--\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
