ba5cca48bb3b2387fdfaf62c3c336f416eb449f3
experiencei <ayelojahighbee01@gmail.com>
2023-11-30 19:15:05 +0100

    working on exercise with interface and error hadluing

--- demo/errors/errors.go
+++ demo/errors/errors.go
@@ -16,3 +16,13 @@ func (s *Stuff) Get(index int) (int, error) {
 		return s.values[index], nil
 	}
 }
+
+func main() {
+	stuff := Stuff{}
+	value, err := stuff.Get(1)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value is", value)
+	}
+}
