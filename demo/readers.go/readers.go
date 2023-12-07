f1a2c36e0c97788815ea5664bc18b1dcd73641aa
experiencei <ayelojahighbee01@gmail.com>
2023-12-07 16:34:32 +0100

    working with readers , writers and bufio

--- demo/readers.go/readers.go
+++ demo/readers.go/readers.go
@@ -8,3 +8,20 @@ import (
 	"strconv"
 	"strings"
 )
+
+func main() {
+	scanner := bufio.NewReader(os.Stdin)
+
+	sum := 0
+
+	for {
+		input, inputErr := scanner.ReadString(' ')
+
+		num, convErr := strconv.Atoi(strings.TrimSpace(input))
+		if convErr != nil {
+			fmt.Println(convErr)
+		} else {
+			sum += num
+		}
+
+		
