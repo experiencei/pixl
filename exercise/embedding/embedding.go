63b166b1395d7dd8604eaf93784a44aded5e13c0
experiencei <ayelojahighbee01@gmail.com>
2023-12-07 17:15:50 +0100

    working with interface and structure embedding in go

---
+++ exercise/embedding/embedding.go
@@ -0,0 +1,11 @@
+//--Summary:
+//  Create a system monitoring dashboard using the existing dashboard
+//  component structures. Each array element in the components represent
+//  a 1-second sampling.
+//
+//--Requirements:
+//* Create functions to calculate averages for each dashboard component
+//* Using struct embedding, create a Dashboard structure that contains
+//  each dashboard component
+//* Print out a 5-second average from each component using promoted
+//  methods on the Dashboard
