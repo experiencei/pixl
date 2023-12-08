02777b4a386fb6b0d5e7843bad02c972259bf475
experiencei <ayelojahighbee01@gmail.com>
2023-12-08 18:27:02 +0100

    working with concurrent programming goroutines

--- exercise/goroutines/goroutines.go
+++ exercise/goroutines/goroutines.go
@@ -57,3 +57,33 @@ func sumFile(rd bufio.Reader) int {
 		sum += num
 	}
 }
+
+func main() {
+	files := []string{"num1.txt", "num2.txt", "num3.txt", "num4.txt", "num5.txt"}
+	sum := 0
+
+	for i := 0; i < len(files); i++ {
+		file, err := os.Open(files[i])
+		if err != nil {
+			//* Report any errors to the terminal
+			fmt.Println("Error:", err)
+			return
+		}
+
+		rd := bufio.NewReader(file)
+
+		calculate := func() {
+			fileSum := sumFile(*rd)
+			//* Add each sum together to get a grand total for all files
+			sum += fileSum
+		}
+		//* Launch a goroutine for each file
+		go calculate()
+	}
+
+	// Need to wait for goroutines to finish. Increase if needed.
+	time.Sleep(100 * time.Millisecond)
+
+	//  - Print the grand total to the terminal
+	fmt.Println(sum)
+}
