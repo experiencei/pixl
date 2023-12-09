6f4d61894737ebb15784188c4c153b4094d74e03
experiencei <ayelojahighbee01@gmail.com>
2023-12-09 21:23:47 +0100

    working with concurrent programming goroutines

--- demo/channels/channels.go
+++ demo/channels/channels.go
@@ -14,3 +14,23 @@ const (
 	DoExit = iota
 	ExitOk
 )
+
+func doubler(jobs, results chan Job, control chan ControlMsg) {
+	for {
+		select {
+		case msg := <-control:
+			switch msg {
+			case DoExit:
+				fmt.Println("Exit goroutine")
+				control <- ExitOk
+				return
+			default:
+				panic("Unhandled control message")
+			}
+		case job := <-jobs:
+			results <- Job{data: job.data, result: job.data * 2}
+		default:
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
