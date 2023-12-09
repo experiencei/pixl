9d5b310e793a389711911c08dcce0878328813d4
experiencei <ayelojahighbee01@gmail.com>
2023-12-09 21:38:31 +0100

    using channels, goroutines to create jobs

--- exercise/channels/channels.go
+++ exercise/channels/channels.go
@@ -19,3 +19,18 @@ import "time"
 import "math/rand"
 
 type Job int
+
+func longCalculation(i Job) int {
+	duration := time.Duration(rand.Intn(1000)) * time.Millisecond
+	time.Sleep(duration)
+	fmt.Printf("Job %d complete in %v\n", i, duration)
+	return int(i) * 30
+}
+
+func makeJobs() []Job {
+	jobs := make([]Job, 0, 100)
+	for i := 0; i < 100; i++ {
+		jobs = append(jobs, Job(rand.Intn(10000)))
+	}
+	return jobs
+}
