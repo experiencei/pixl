504136dbe9f5fb20f6de7107bd523459c31e7caf
experiencei <ayelojahighbee01@gmail.com>
2023-12-09 22:51:33 +0100

    using channels, goroutines to create jobs, waitgroups

---
+++ demo/mutexeex/mutexes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func wait() {
+	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+}
+
+type Hits struct {
+	count int
+	sync.Mutex
+}
