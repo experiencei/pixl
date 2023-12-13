b4e8dd03b749f39558072fe3bb0c4f8b1b9b4cd3
experiencei <ayelojahighbee01@gmail.com>
2023-12-13 22:34:16 +0100

    working with syncronization

---
+++ exercise/sync/sync.go
@@ -0,0 +1,11 @@
+//--Summary:
+//  Create a program that can read text from standard input and count the
+//  number of letters present in the input.
+//
+//--Requirements:
+//* Count the total number of letters in any chosen input
+//* The input must be supplied from standard input
+//* Input analysis must occur per-word, and each word must be analyzed
+//  within a goroutine
+//* When the program finishes, display the total number of letters counted
+//
