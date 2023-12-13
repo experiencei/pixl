c906e525a231302e07d8bd8ccd87c9971040b718
experiencei <ayelojahighbee01@gmail.com>
2023-12-13 22:56:45 +0100

    working with syncronization

--- exercise/sync/sync.go
+++ exercise/sync/sync.go
@@ -46,3 +46,44 @@ func countLetters(word string) int {
 	}
 	return letters
 }
+
+func main() {
+	//* The input must be supplied from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	totalLetters := Count{}
+
+	var wg sync.WaitGroup
+
+	for {
+		if scanner.Scan() {
+			line := scanner.Text()
+			words := getWords(line)
+			for _, word := range words {
+				wordCopy := word
+				wg.Add(1)
+				//* Input analysis must occur per-word, and each word must be analyzed
+				//  within a goroutine
+				go func() {
+					totalLetters.Lock()
+					defer totalLetters.Unlock()
+					defer wg.Done()
+					sum := countLetters(wordCopy)
+					totalLetters.count += sum
+				}()
+			}
+		} else {
+			if scanner.Err() == nil {
+				break
+			}
+		}
+	}
+	wg.Wait()
+
+	//* When the program finishes, display the total number of letters counted
+	totalLetters.Lock()
+	sum := totalLetters.count
+	totalLetters.Unlock()
+
+	fmt.Println("total letters =", sum)
+}
