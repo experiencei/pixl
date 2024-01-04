87383550ff009629b6c22ca80ebbbb7c4d94db9e
experiencei <ayelojahighbee01@gmail.com>
2024-01-04 14:06:36 +0100

    working with with scope and understanding the nuance of it

--- demo/scope/scope.go
+++ demo/scope/scope.go
@@ -7,12 +7,12 @@ package main
 
 // import statements are file scoped
 import (
-    "fmt"
+	"fmt"
 
-    // import "fmt" -> error, within the same scope, unique names
+	// import "fmt" -> error, within the same scope, unique names
 
-    // importing as another name (alias) is permitted
-    f "fmt"
+	// importing as another name (alias) is permitted
+	f "fmt"
 )
 
 // variables or constant declared outside any function are package scoped
@@ -20,17 +20,17 @@ const done = false
 
 func main() { // package scoped
 
-    // block scoped: visible until the end of the block "}"
-    var task = "Running:"
-    fmt.Println(task, done) // => Running: false (this is done from package scope)
-    f.Println("Bye bye!")
+	// block scoped: visible until the end of the block "}"
+	var task = "Running:"
+	fmt.Println(task, done) // => Running: false (this is done from package scope)
+	f.Println("Bye bye!")
 
-    // names must be unique only within the same scope
-    const done = true                        // local scoped
-    fmt.Printf("done in main(): %v\n", done) // => done in main(): true
-    f1()
+	// names must be unique only within the same scope
+	const done = true                        // local scoped
+	fmt.Printf("done in main(): %v\n", done) // => done in main(): true
+	f1()
 }
 
 func f1() {
-    fmt.Printf("done in f(): %v\n", done) //this is done from package scope
+	fmt.Printf("done in f(): %v\n", done) //this is done from package scope
 }
