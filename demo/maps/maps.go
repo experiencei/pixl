11d94944ef6994238c8eeed6018991536faca726
experiencei <ayelojahighbee01@gmail.com>
2024-01-09 07:27:19 +0100

    compring maps in go with Sprintf

--- demo/maps/maps.go
+++ demo/maps/maps.go
@@ -30,4 +30,26 @@ func main() {
 		totalItems += amount
 	}
 	fmt.Println("There are", totalItems, "on the shopping list")
+
+
+	//** COMPARING MAPS **//
+
+    // Maps cannot be compared using == operator. A map can be compared only to nil.
+    a := map[string]string{"A": "X"}
+    b := map[string]string{"B": "X"}
+
+    // fmt.Println(a == b) // error -> invalid operation: a == b (map can only be compared to nil)
+
+    // to compare 2 maps that have the keys and values of type string
+    // we get a string representation of the maps and compare those strings.
+
+    // getting a string representation of maps called a and b
+    s1 := fmt.Sprintf("%s", a)
+    s2 := fmt.Sprintf("%s", b)
+
+    if s1 == s2 {
+        fmt.Println("Maps are equal")
+    } else {
+        fmt.Println("Maps are not equal")
+    }
 }
