69bdff293ee089f93e68e1d49b2b2f8ed1243366
experiencei <ayelojahighbee01@gmail.com>
2024-01-17 11:36:21 +0100

    working with variadics function and defer

--- demo/function/functions.go
+++ demo/function/functions.go
@@ -16,6 +16,44 @@ func double(x int) int  {
 	return x + x
 }
 
+
+// Variadic functions are functions that take a variable number of arguments.
+// Ellipsis prefix (three-dots) in front of the parameter type makes a function variadic.
+// The function may be called with zero or more arguments for that parameter.
+// If the function takes parameters of different types, only the last parameter of a function can be variadic.
+
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+// creating a variadic function
+func f1(a ...int) {
+    fmt.Printf("%T\n", a) // => []int, slice of int
+    fmt.Printf("%#v\n", a)
+}
+
+// variadicfunction that modifies one of the arguments passed.
+func f2(a ...int) {
+    a[0] = 50
+}
+
+// creating a variadic function that calculates and returns the sum and product of its arguments
+func sumAndProduct(a ...float64) (float64, float64) {
+    sum := 0.
+    product := 1.
+
+    for _, v := range a {
+        sum += v
+        product *= v
+    }
+
+    return sum, product
+}
+
+
 func main() {
 	greet()
  
