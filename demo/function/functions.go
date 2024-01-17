31462775671f2cc1f42cb8c4a9065bc1e76bd22a
experiencei <ayelojahighbee01@gmail.com>
2024-01-17 11:38:53 +0100

    working with variadics function , defer and anonymous func

--- demo/function/functions.go
+++ demo/function/functions.go
@@ -92,3 +92,25 @@ func main() {
 
 }
 
+
+
+// function that takes an int as an argument and returns another function that returns an int
+func increment(x int) func() int {
+	return func() int {
+			x++
+			return x
+	}
+}
+
+func mainII() {
+	// declaring an anonymous functions
+	func(msg string) {
+			fmt.Println(msg)
+	}("I'm an anonymous function!") // calling the anonymous function
+
+	// calling the increment function. It returns an anonymous function
+	a := increment(10)
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+}
